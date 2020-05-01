21e6433ad8c38ea7b7ce40748e147b8963a06cca
carterbancroft <carter.bancroft@gmail.com>
2020-04-30 18:21:25 -0600

    Adjust some game settings

--- js/game.js
+++ js/game.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const cellSize = 10
+const cellSize = 3
 const padding = 1
-const rows = 50
-const cols = 100
-const lifeThreshold = 0.92
+const rows = 200
+const cols = 350
+const lifeThreshold = 0.93
 
 const world = new World(rows, cols, lifeThreshold)
 
 const canvas = document.getElementById('canvas')
-const width = (rows * cellSize) + (rows * padding)
-const height = (cols * cellSize) + (cols * padding)
+const width = (cols * cellSize) + (cols * padding)
+const height = (rows * cellSize) + (rows * padding)
 
 canvas.width = width
 canvas.height = height
@@ -19,34 +19,32 @@ function drawWorld() {
   const context = canvas.getContext('2d')
   context.clearRect(0, 0, width, height)
 
-  let yPos = cellSize / 2
+  let yPos = 0
   for (let r = 0; r < world.rows; r++) {
-    let xPos = cellSize / 2
+    let xPos = 0
 
     for (let c = 0; c < world.cols; c++) {
       const cell = world.grid[r][c]
 
       if (cell.state === cellStates.dead) {
-        context.fillStyle = "#FF0000"
+        context.fillStyle = "#ffffff"
       }
       else {
-        context.fillStyle = "#0000FF"
+        context.fillStyle = "#0000ff"
       }
 
       context.fillRect(xPos, yPos, cellSize, cellSize)
-      xPos += cellSize + 1
+      xPos += cellSize + padding
     }
 
-    yPos += cellSize + 1
+    yPos += cellSize + padding
   }
 }
 
 let i = 0
 setInterval(() => {
-  console.log(`generation: ${i}`)
-
   world.update()
   drawWorld()
 
   i++
-}, 200)
+}, 100)
